e5517725d1c4005c0710ed8dd8b374cf6c875e8e
Nahid Hasan <nahidthenh@gmail.com>
2021-02-02 16:55:46 +0600

    MOPO PSD

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function($){
+	// "use strict";
+	
+	
+	$('.video-play').magnificPopup({
+		type: 'iframe'
+	});
+	
+	
+	
+	
+	
+	
+	
+	/* scrollUp Minimum setup */
+	$(function () {
+		$.scrollUp();
+	});
+	
+}(jQuery));
